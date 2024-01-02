b4bf26b35c45f9be6014c4bd5e45793741935c01
Manolis Pantelakis <manospantelak@gmail.com>
2024-01-02 22:15:55 +0200

    Finished Login Page and Small Fix in widgets
    Fixed in widgets/custom_text_form_field_data autofocus initializing it to false as it was opening the keyboard by default

--- lib/presentation/login_page_screen/login_page_screen.dart
+++ lib/presentation/login_page_screen/login_page_screen.dart
@@ -1,134 +1,146 @@
 import 'package:flutter/material.dart';
 import 'package:stratos_s_application3/core/app_export.dart';
-import 'package:stratos_s_application3/widgets/custom_outlined_button.dart';
 import 'package:stratos_s_application3/widgets/custom_text_form_field.dart';
 
 // ignore_for_file: must_be_immutable
-class LoginPageScreen extends StatelessWidget {
+class LoginPageScreen extends StatefulWidget {
   LoginPageScreen({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPageScreen> createState() => _LoginPageScreenState();
+}
+
+class _LoginPageScreenState extends State<LoginPageScreen> {
   TextEditingController emailController = TextEditingController();
 
   TextEditingController passwordController = TextEditingController();
 
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  bool obscureText = true;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
             backgroundColor: theme.colorScheme.primary,
-            resizeToAvoidBottomInset: false,
-            body: Form(
-                key: _formKey,
-                child: Container(
-                    width: double.maxFinite,
-                    padding: EdgeInsets.only(top: 81.v),
-                    child: Column(children: [
-                      SizedBox(
-                          height: 131.v,
-                          width: 136.h,
-                          child: Stack(alignment: Alignment.center, children: [
-                            Align(
-                                alignment: Alignment.center,
-                                child: Container(
-                                    height: 131.v,
-                                    width: 136.h,
-                                    decoration: BoxDecoration(
-                                        color: theme.colorScheme.onPrimary
-                                            .withOpacity(1),
-                                        borderRadius:
-                                            BorderRadius.circular(68.h)))),
-                            CustomImageView(
-                                imagePath: ImageConstant.imgImage1,
-                                height: 131.v,
-                                width: 136.h,
-                                radius: BorderRadius.vertical(
-                                    bottom: Radius.circular(5.h)),
-                                alignment: Alignment.center)
-                          ])),
-                      SizedBox(height: 36.v),
-                      Container(
-                          decoration: AppDecoration.outlineBlack,
-                          child: Text("ECE Library",
-                              style: theme.textTheme.headlineLarge)),
-                      SizedBox(height: 38.v),
-                      CustomTextFormField(
-                          width: 210.h,
-                          controller: emailController,
-                          hintText: "Email",
-                          textInputType: TextInputType.emailAddress),
-                      SizedBox(height: 35.v),
-                      CustomTextFormField(
-                          width: 210.h,
-                          controller: passwordController,
-                          hintText: "Password",
-                          textInputAction: TextInputAction.done,
-                          textInputType: TextInputType.visiblePassword,
-                          suffix: Container(
-                              margin:
-                                  EdgeInsets.fromLTRB(30.h, 16.v, 21.h, 16.v),
-                              child: CustomImageView(
-                                  imagePath: ImageConstant.imgEye,
-                                  height: 24.adaptSize,
-                                  width: 24.adaptSize)),
-                          suffixConstraints: BoxConstraints(maxHeight: 56.v),
-                          obscureText: true,
-                          contentPadding: EdgeInsets.only(
-                              left: 16.h, top: 18.v, bottom: 18.v)),
-                      SizedBox(height: 22.v),
-                      CustomOutlinedButton(
-                          height: 40.v,
-                          width: 101.h,
-                          text: "Login",
-                          leftIcon: Container(
-                              margin: EdgeInsets.only(right: 8.h),
-                              child: CustomImageView(
-                                  imagePath: ImageConstant.imgArrowright,
-                                  height: 18.adaptSize,
-                                  width: 18.adaptSize)),
-                          buttonStyle: CustomButtonStyles.outlineGrayE,
-                          buttonTextStyle: CustomTextStyles.titleSmallGray90001,
-                          onPressed: () {
-                            onTapLogin(context);
-                          }),
-                      SizedBox(height: 43.v),
-                      Align(
-                          alignment: Alignment.centerLeft,
-                          child: SizedBox(
-                              height: 39.v,
-                              width: 225.h,
-                              child: Stack(
-                                  alignment: Alignment.centerRight,
-                                  children: [
-                                    Align(
-                                        alignment: Alignment.centerLeft,
-                                        child: Text("Don’t have an account? ",
-                                            style: CustomTextStyles
-                                                .bodyLargeBlack900)),
-                                    Align(
-                                        alignment: Alignment.centerRight,
-                                        child: GestureDetector(
-                                            onTap: () {
-                                              onTapTxtSignUp(context);
-                                            },
-                                            child: Container(
-                                                width: 62.h,
-                                                padding: EdgeInsets.symmetric(
-                                                    horizontal: 4.h,
-                                                    vertical: 8.v),
-                                                decoration: AppDecoration
-                                                    .fillPrimary
-                                                    .copyWith(
-                                                        borderRadius:
-                                                            BorderRadiusStyle
-                                                                .roundedBorder10),
-                                                child: Text("Sign up",
-                                                    style: CustomTextStyles
-                                                        .bodyLargeOnPrimary))))
-                                  ]))),
-                      SizedBox(height: 5.v)
-                    ])))));
+            body: SingleChildScrollView(
+              child: Column(
+                  //crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: double.maxFinite,
+                      padding: EdgeInsets.only(top: 50.v),
+                      child: Stack(alignment: Alignment.center, children: [
+                        Container(
+                            width: 170.v,
+                            height: 170.h,
+                            decoration: BoxDecoration(
+                              color: theme.colorScheme.onPrimary.withOpacity(1),
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  image: AssetImage(ImageConstant.imgImage1)),
+                              border: Border.all(width: 2),
+                            )),
+                      ]),
+                    ),
+                    Container(
+                        decoration: AppDecoration.outlineBlack,
+                        child: Text("ECE Library",
+                            style: theme.textTheme.headlineLarge)),
+                    Form(
+                        key: _formKey,
+                        child: Container(
+                          padding: EdgeInsets.only(top: 40.v),
+                          child: Column(children: [
+                            CustomTextFormField(
+                                width: 210.h,
+                                controller: emailController,
+                                hintText: "E-mail",
+                                textInputType: TextInputType.emailAddress),
+                            Container(
+                              padding: EdgeInsets.only(top: 40.v),
+                            ),
+                            CustomTextFormField(
+                                width: 210.h,
+                                controller: passwordController,
+                                hintText: "Password",
+                                textInputAction: TextInputAction.done,
+                                obscureText: obscureText,
+                                textInputType: TextInputType.visiblePassword,
+                                suffix: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      obscureText = !obscureText;
+                                    });
+                                  },
+                                  child: Icon(
+                                    obscureText
+                                        ? Icons.remove_red_eye_outlined
+                                        : Icons.remove_red_eye,
+                                    color: appTheme.blueGray100,
+                                  ),
+                                )),
+                            Container(
+                              padding: EdgeInsets.only(top: 40.v),
+                            ),
+                            OutlinedButton(
+                              onPressed: () {
+                                onTapLogin(context);
+                              },
+                              style: OutlinedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                side: BorderSide(color: appTheme.blueGray100),
+                                fixedSize: Size(101, 40),
+                                backgroundColor: appTheme.blueGray100,
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.arrow_forward,
+                                      color: theme.colorScheme.primary),
+                                  Text(
+                                    "Login",
+                                    style: TextStyle(
+                                        fontSize: 14.0, color: Colors.black),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ]),
+                        )),
+                    Container(
+                      padding: EdgeInsets.only(top: 20.v),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don’t have an account? ",
+                          style: CustomTextStyles.bodyLargeBlack900
+                              .copyWith(fontSize: 14.0),
+                        ),
+                        GestureDetector(
+                            onTap: () {
+                              onTapTxtSignUp(context);
+                            },
+                            child: Container(
+                                width: 62.h,
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 4.h, vertical: 8.v),
+                                decoration: AppDecoration.fillPrimary.copyWith(
+                                    borderRadius:
+                                        BorderRadiusStyle.roundedBorder10),
+                                child: Text("Sign up",
+                                    style: CustomTextStyles.bodyLargeOnPrimary
+                                        .copyWith(fontSize: 15.0))))
+                      ],
+                    )
+                  ]),
+            )));
   }
 
   /// Navigates to the homePageContainerScreen when the action is triggered.
--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -8,7 +8,7 @@ class CustomTextFormField extends StatelessWidget {
     this.width,
     this.controller,
     this.focusNode,
-    this.autofocus = true,
+    this.autofocus = false,
     this.textStyle,
     this.obscureText = false,
     this.textInputAction = TextInputAction.next,
