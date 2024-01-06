8443aa21029c6cfac09d6d652a7bcbaf667aa1ec
Manolis Pantelakis <manospantelak@gmail.com>
2024-01-06 02:56:27 +0200

    Completed FAQs Page

--- lib/presentation/faqs_page_screen/faqs_page_screen.dart
+++ lib/presentation/faqs_page_screen/faqs_page_screen.dart
@@ -3,15 +3,64 @@ import 'package:stratos_s_application3/core/app_export.dart';
 import 'package:stratos_s_application3/widgets/app_bar/appbar_leading_image.dart';
 import 'package:stratos_s_application3/widgets/app_bar/appbar_subtitle.dart';
 import 'package:stratos_s_application3/widgets/app_bar/custom_app_bar.dart';
-import 'package:stratos_s_application3/widgets/custom_text_form_field.dart';
 
-// ignore_for_file: must_be_immutable
-class FaqsPageScreen extends StatelessWidget {
+class Item {
+  Item({
+    required this.id,
+    required this.expandedText,
+    required this.headerText,
+  });
+  int id;
+  final expandedText;
+  final String headerText;
+}
+
+class FaqsPageScreen extends StatefulWidget {
   FaqsPageScreen({Key? key}) : super(key: key);
 
-  TextEditingController emailController = TextEditingController();
+  @override
+  State<FaqsPageScreen> createState() => _FaqsPageScreenState();
+}
 
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+class _FaqsPageScreenState extends State<FaqsPageScreen> {
+  final List<Item> faqs = <Item>[
+    Item(
+        id: 1,
+        expandedText:
+            "Η βιβλιοθήκη είναι ανοικτή τις ημέρες που είναι ανοικτή η σχολή με ωράριο 09:00 - 18:00. Σε περίοδο διακοπών, η Βιβλιοθήκη λειτουργεί τις ημέρες που είναι ανοιχτές και οι υπόλοιπες Διοικητικές Υπηρεσίες της Σχολής με ωράριο 09:00-15:00. Σε περίπτωση κλειστής σχολής (λόγω κατάληψης, απεργίας ή διακοπών) η βιβλιοθήκη παραμένει κλειστή. Σε κάθε περίπτωση θα αναρτάται στην αρχική σελίδα στο site της Βιβλιοθήκης έγκαιρα, σχετική ενημέρωση για την εξυπηρέτηση του κοινού.",
+        headerText:
+            "Ποιές μέρες η βιβλιοθήκη είναι κλειστή, πέραν του μόνιμου ωραρίου; Πότε είναι ανοικτή η βιβλιοθήκη;"),
+    Item(
+        id: 2,
+        expandedText:
+            "Η πολιτική που ακολουθούμε στο συγκεκριμένο ζήτημα αφορά την ομαλή λειτουργία της βιβλιοθήκης και την τήρηση των τυπικών διαδικασιών. Η βιβλιοθήκη παρέχει φωτοτυπικό για σημειώσεις για την εξυπηρέτηση των φοιτητών.Υπάρχει επίσης η δυνατότητα εκτύπωσης μέσω USB ή email/cloud. Δεν αναλαμβάνουμε, ωστόστο, scans αρχείων, εκτός αν πρόκειται για σημειώσεις που θα ανέβουν στο forum της Σχολής.",
+        headerText:
+            "Μπορώ να τυπώσω από USB stick ή από το προσωπικό μου e-mail / cloud;"),
+    Item(
+        id: 3,
+        expandedText:
+            "Η απόφασή μας στο συγκεκριμένο ζήτημα αφορά την ίση και δίκαιη συμπεριφορά μας ως βιβλιοθήκη απέναντι στο σύνολο των φοιτητών, και δεν έχει «εκδικητικό» χαρακτήρα. Το βιβλίο που ένας φοιτητής καθυστέρησε να επιστρέψει, ίσως ήταν απαραίτητο για κάποιον άλλο φοιτητή, ειδικά σε περίοδο εξεταστικής. Έτσι εξασφαλίζεται η διμερής συνέπεια ανάμεσα σε βιβλιοθήκη και δανειζόμενο, καθώς και η τήρηση των προβλεπόμενων ημερομηνιών / κανόνων, πράγμα αυτονόητο.",
+        headerText:
+            "Γιατί να τιμωρηθώ επειδή καθυστέρησα να επιστρέψω το βιβλίο που δανείστηκα;"),
+    Item(
+        id: 4,
+        expandedText:
+            "Δικαίωμα υποβολής βιογραφικού για εργασία στη βιβλιοθήκη έχει κάθε φοιτητής της Σχολής ΗΜΜΥ, ωστόσο συνήθως προτιμώνται άτομα από μικρότερα έτη ώστε να διατηρείται ένας κορμός στη βιβλιοθήκη. Οι προσλήψεις γίνονται μία φορά περίπου το χρόνο. Σχετική ενημέρωση ανεβαίνει και στο site μας, όσο και στην επίσημη ιστοσελίδα της Σχολής ΗΜΜΥ.",
+        headerText:
+            "Μπορώ να κάνω αίτηση για να εργαστώ κι εγώ στη βιβλιοθήκη;"),
+    Item(
+        id: 5,
+        expandedText:
+            "Η απόφαση αυτή έχει παρθεί για την διασφάλιση των προσωπικών σας αντικειμένων, την αποτροπή κλοπών και την αποφυγή «μπερδέματος» (π.χ. κάποιος πήρε την ίδια μαύρη polo συμφοιτητή του). Επίσης δεν υπάρχει επαρκής χώρος στο αναγνωστήριο.",
+        headerText:
+            "Γιατί δεν μπορώ να πάρω την τσάντα μου μαζί μου στο αναγνωστήριο;"),
+    Item(
+        id: 6,
+        expandedText:
+            "Δεν επιτρέπονται οι καφέδες δεδομένου ότι έχουν συμβεί κατά καιρούς «ατυχήματα» με συνέπεια: την ρύπανση του χώρου, καταστροφή βιβλίων, πιθανά προβλήματα με τις πρίζες τροφοδοσίας ρεύματος και βλάβη σε laptop του ιδίου ή συμφοιτητή του. Μπορείτε να έχετε μαζί σας μόνο νερό σε μπουκαλάκι ή θερμός.",
+        headerText:
+            "Γιατί δεν μπορώ να πάρω τον καφέ μαζί μου στο αναγνωστήριο;")
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -19,47 +68,42 @@ class FaqsPageScreen extends StatelessWidget {
         child: Scaffold(
             resizeToAvoidBottomInset: false,
             appBar: _buildAppBar(context),
-            body: Form(
-                key: _formKey,
-                child: SizedBox(
-                    width: double.maxFinite,
-                    child: Column(children: [
-                      Container(
-                          height: 28.v,
-                          width: double.maxFinite,
-                          decoration:
-                              BoxDecoration(color: appTheme.blueGray100)),
-                      SizedBox(height: 16.v),
-                      _buildDropContent1(context),
-                      SizedBox(height: 25.v),
-                      Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 6.h),
-                          child: CustomTextFormField(
-                              controller: emailController,
-                              hintText:
-                                  "Μπορώ να τυπώσω από USB stick ή από το προσωπικό μου e-mail / cloud;",
-                              hintStyle: CustomTextStyles.titleSmallBluegray100,
-                              textInputAction: TextInputAction.done,
-                              textInputType: TextInputType.emailAddress,
-                              contentPadding: EdgeInsets.symmetric(
-                                  horizontal: 20.h, vertical: 19.v),
-                              borderDecoration:
-                                  TextFormFieldStyleHelper.fillPrimary,
-                              filled: true,
-                              fillColor: theme.colorScheme.primary)),
-                      SizedBox(height: 25.v),
-                      _buildDropContent2(context),
-                      SizedBox(height: 25.v),
-                      _buildDropContent3(context),
-                      SizedBox(height: 25.v),
-                      _buildDropContent4(context),
-                      SizedBox(height: 25.v),
-                      _buildDropContent5(context),
-                      SizedBox(height: 5.v)
-                    ])))));
+            body: Padding(
+              padding: EdgeInsets.only(
+                  top: 30.adaptSize,
+                  left: 15.adaptSize,
+                  right: 15.adaptSize,
+                  bottom: 30.adaptSize),
+              child: ListView(children: [
+                ExpansionPanelList.radio(
+                  elevation: 4,
+                  animationDuration: Duration(milliseconds: 750),
+                  expandIconColor: appTheme.blueGray100,
+                  dividerColor: appTheme.blueGray100,
+                  expandedHeaderPadding: EdgeInsets.only(top: 4.adaptSize),
+                  children: faqs.map<ExpansionPanelRadio>((Item item) {
+                    return ExpansionPanelRadio(
+                      value: item.id,
+                      headerBuilder: (BuildContext context, bool isExpanded) {
+                        return ListTile(
+                          title: Text(item.headerText,
+                              style: TextStyle(color: appTheme.blueGray100)),
+                        );
+                      },
+                      backgroundColor: theme.colorScheme.primary,
+                      body: ListTile(
+                        title: Text(
+                          item.expandedText,
+                          style: TextStyle(color: appTheme.blueGray100),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ]),
+            )));
   }
 
-  /// Section Widget
   PreferredSizeWidget _buildAppBar(BuildContext context) {
     return CustomAppBar(
         leadingWidth: 30.h,
@@ -72,164 +116,6 @@ class FaqsPageScreen extends StatelessWidget {
         title:
             AppbarSubtitle(text: "FAQs", margin: EdgeInsets.only(left: 14.h)),
         styleType: Style.bgFill);
-  }
-
-  /// Section Widget
-  Widget _buildDropContent1(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.symmetric(horizontal: 6.h),
-        padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 10.v),
-        decoration: AppDecoration.fillPrimary
-            .copyWith(borderRadius: BorderRadiusStyle.roundedBorder10),
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Expanded(
-                  child: Container(
-                      width: 244.h,
-                      margin: EdgeInsets.only(bottom: 8.v),
-                      child: Text(
-                          "Ποιές μέρες η βιβλιοθήκη είναι κλειστή, πέραν του μόνιμου ωραρίου; Πότε είναι ανοικτή η βιβλιοθήκη;",
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
-                          style: CustomTextStyles.titleSmallBluegray100
-                              .copyWith(height: 1.43)))),
-              Container(
-                  height: 50.adaptSize,
-                  width: 50.adaptSize,
-                  margin: EdgeInsets.only(left: 13.h, top: 4.v, bottom: 14.v),
-                  decoration: BoxDecoration(
-                      color: appTheme.blueGray100,
-                      borderRadius: BorderRadius.circular(25.h)))
-            ]));
-  }
-
-  /// Section Widget
-  Widget _buildDropContent2(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.symmetric(horizontal: 6.h),
-        padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 10.v),
-        decoration: AppDecoration.fillPrimary
-            .copyWith(borderRadius: BorderRadiusStyle.roundedBorder10),
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                  width: 203.h,
-                  margin: EdgeInsets.only(bottom: 8.v),
-                  child: Text(
-                      "Γιατί να τιμωρηθώ επειδή καθυστέρησα να επιστρέψω το βιβλίο που δανείστηκα;",
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style: CustomTextStyles.titleSmallBluegray100
-                          .copyWith(height: 1.43))),
-              Container(
-                  height: 50.adaptSize,
-                  width: 50.adaptSize,
-                  margin: EdgeInsets.only(left: 54.h, top: 4.v, bottom: 14.v),
-                  decoration: BoxDecoration(
-                      color: appTheme.blueGray100,
-                      borderRadius: BorderRadius.circular(25.h)))
-            ]));
-  }
-
-  /// Section Widget
-  Widget _buildDropContent3(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.symmetric(horizontal: 6.h),
-        padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 10.v),
-        decoration: AppDecoration.fillPrimary
-            .copyWith(borderRadius: BorderRadiusStyle.roundedBorder10),
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                  width: 213.h,
-                  margin: EdgeInsets.only(top: 5.v, bottom: 13.v),
-                  child: Text(
-                      "Μπορώ να κάνω αίτηση για να εργαστώ κι εγώ στη βιβλιοθήκη;",
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: CustomTextStyles.titleSmallBluegray100
-                          .copyWith(height: 1.43))),
-              Container(
-                  height: 50.adaptSize,
-                  width: 50.adaptSize,
-                  margin: EdgeInsets.only(left: 44.h, bottom: 10.v),
-                  decoration: BoxDecoration(
-                      color: appTheme.blueGray100,
-                      borderRadius: BorderRadius.circular(25.h)))
-            ]));
-  }
-
-  /// Section Widget
-  Widget _buildDropContent4(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.symmetric(horizontal: 6.h),
-        padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 10.v),
-        decoration: AppDecoration.fillPrimary
-            .copyWith(borderRadius: BorderRadiusStyle.roundedBorder10),
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Expanded(
-                  child: Container(
-                      width: 240.h,
-                      margin: EdgeInsets.only(top: 5.v, bottom: 13.v),
-                      child: Text(
-                          "Γιατί δεν μπορώ να πάρω την τσάντα μου μαζί μου στο αναγνωστήριο;",
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: CustomTextStyles.titleSmallBluegray100
-                              .copyWith(height: 1.43)))),
-              Container(
-                  height: 50.adaptSize,
-                  width: 50.adaptSize,
-                  margin: EdgeInsets.only(left: 17.h, bottom: 10.v),
-                  decoration: BoxDecoration(
-                      color: appTheme.blueGray100,
-                      borderRadius: BorderRadius.circular(25.h)))
-            ]));
-  }
-
-  /// Section Widget
-  Widget _buildDropContent5(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.symmetric(horizontal: 6.h),
-        padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 10.v),
-        decoration: AppDecoration.fillPrimary
-            .copyWith(borderRadius: BorderRadiusStyle.roundedBorder10),
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Expanded(
-                  child: Container(
-                      width: 228.h,
-                      margin: EdgeInsets.only(top: 5.v, bottom: 13.v),
-                      child: Text(
-                          "Γιατί δεν μπορώ να πάρω τον καφέ μαζί μου στο αναγνωστήριο;",
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: CustomTextStyles.titleSmallBluegray100
-                              .copyWith(height: 1.43)))),
-              Container(
-                  height: 50.adaptSize,
-                  width: 50.adaptSize,
-                  margin: EdgeInsets.only(left: 29.h, bottom: 10.v),
-                  decoration: BoxDecoration(
-                      color: appTheme.blueGray100,
-                      borderRadius: BorderRadius.circular(25.h)))
-            ]));
   }
 
   /// Navigates back to the previous screen.
